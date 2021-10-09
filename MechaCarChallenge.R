9b0dcef8ba1243f4e1d54825eb468fbe590dc359
Steve King <sking@Steves-MacBook-Pro.local>
2021-10-08 20:05:40 -0400

    RScript

---
+++ MechaCarChallenge.R
@@ -0,0 +1,30 @@
+#Deliverable 1: Linear Regression to Predict MPG
+
+library(dplyr)
+library(tidyverse)
+
+#  Read in the .csv file.
+MechaCar_data <- read.csv(file="MechaCar_mpg.csv",check.names = F,stringsAsFactors = F)
+
+# Perform linear regression
+reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_data)
+summary(reg)
+
+#Deliverable 2: Summary Statistics on Suspension Coils
+
+#  Read in the .csv file.
+suspension_Coils_data <- read.csv(file="Suspension_Coil.csv",check.names = F,stringsAsFactors = F)
+
+#Create dataframe using summarize() function of the PSI column
+total_summary <- suspension_Coils_data %>% summarize(Mean = mean(PSI), Median=median(PSI), 
+                                                     Variance=var(PSI), SD= sd(PSI), .groups = 'keep' )
+total_summary
+
+#Create dataframe using groupby() and summarize() to group manufactoring lot of the PSI column
+lot_summary <- suspension_Coils_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median=median(PSI), 
+                                                                                   Variance=var(PSI), SD= sd(PSI), .groups = 'keep' )
+lot_summary
+
+
+
+
